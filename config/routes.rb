095af170197a4b852b6183e92505e6e44722cde7
Om Jogani <om080jogani@gmail.com>
2024-03-18 23:03:14 +0530

    Update: Added new Routes with Resources

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,11 @@ Rails.application.routes.draw do
   resources :todos
   get 'todos', to: 'todos#index', as: 'todo_index'
 
-  get 'todos/new', to: 'todos#new', as: 'todo_new'
-  get 'todos/:id', to: 'todos#show', as: 'todo_show'
-
   patch 'todos/:id', to: 'todos#update', as: 'todo_update'
   put 'todos/:id', to: 'todos#update', as: 'todo_put'
+
+  get 'todos/new', to: 'todos#new', as: 'todo_new'
+  get 'todos/:id', to: 'todos#show', as: 'todo_show'
 
   get 'todos/:id/edit', to: 'todos#edit', as: 'todo_edit'
   post 'todos', to: 'todos#create', as: 'todo_create'
